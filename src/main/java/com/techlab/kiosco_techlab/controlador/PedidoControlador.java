f14e4f6fa19c2f992ea5ca70bb4ca197200aee16
maveggia <maveggia@gmail.com>
2026-07-05 17:45:19 -0300

    proyecto final

--- src/main/java/com/techlab/kiosco_techlab/controlador/PedidoControlador.java
+++ src/main/java/com/techlab/kiosco_techlab/controlador/PedidoControlador.java
@@ -1,6 +1,7 @@
 package com.techlab.kiosco_techlab.controlador;
 
 import com.techlab.kiosco_techlab.modelo.Pedido;
+import com.techlab.kiosco_techlab.repositorio.PedidoRepository;
 import com.techlab.kiosco_techlab.servicio.PedidoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,9 @@ public class PedidoControlador {
     @Autowired
     private PedidoServicio pedidoServicio;
 
+    @Autowired
+    private PedidoRepository pedidoRepositorio;
+
     @GetMapping
     public List<Pedido> listar() {
         return pedidoServicio.listar();
@@ -28,5 +32,12 @@ public class PedidoControlador {
     @GetMapping("/{id}")
     public Pedido buscarPorId(@PathVariable Long id) {
         return pedidoServicio.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}/estado")
+    public Pedido actualizarEstado(@PathVariable Long id, @RequestBody String estado) {
+        Pedido pedido = pedidoServicio.buscarPorId(id);
+        pedido.setEstado(estado.replace("\"", ""));
+        return pedidoRepositorio.save(pedido);
     }
 }
